619f1eb64d56e99e60b7ed622529b285f2b5535f
HenryDM <119537238+Henry8K@users.noreply.github.com>
2023-04-08 00:23:40 -0300

    Adicionar JS

--- JS/Pages/regras.js
+++ JS/Pages/regras.js
@@ -9,3 +9,25 @@ navbars.addEventListener('click', () => {
     navbars.classList.toggle('active');
     navMobileMenu.classList.toggle('active');
 });
+
+// ================
+//    COPIAR IP
+// ================
+const copyBtn = document.getElementById("copy-btn");
+
+copyBtn.addEventListener("click", function() {
+    var ip = "jogar.minecket.net"; // Adicione o IP do seu servidor aqui
+
+    navigator.clipboard.writeText(ip)
+        .then(() => {
+            var popup = document.getElementById("play-copy-alert");
+
+            setTimeout(function() {
+                popup.classList.add("show");
+            }, 50);
+
+            setTimeout(function() {
+                popup.classList.remove("show");
+            }, 2000);
+        });
+});
