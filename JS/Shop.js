9bf080123bfe56c659702cc9b562d452902dc3dd
HenryDM <henrythierrydev@gmail.com>
2023-04-11 20:18:03 -0300

    Fix default bugs

--- JS/Shop.js
+++ JS/Shop.js
@@ -1,4 +1,58 @@
 // =====================
+//   BENEFITS POPUPS
+// =====================
+
+function setupPopup(openBtn, closeBtn, popup, overlay) {
+  openBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("popup-active");
+    overlay.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("popup-active");
+    overlay.style.display = "none";
+  });
+}
+
+const popup1 = document.getElementById("benefits-1");
+const overlay1 = document.querySelector(".overlay");
+setupPopup(
+  document.getElementById("open-popup-1"),
+  document.getElementById("close-popup-1"),
+  popup1,
+  overlay1
+);
+
+const popup2 = document.getElementById("benefits-2");
+const overlay2 = document.querySelector(".overlay");
+setupPopup(
+  document.getElementById("open-popup-2"),
+  document.getElementById("close-popup-2"),
+  popup2,
+  overlay2
+);
+
+const popup3 = document.getElementById("benefits-3");
+const overlay3 = document.querySelector(".overlay");
+setupPopup(
+  document.getElementById("open-popup-3"),
+  document.getElementById("close-popup-3"),
+  popup3,
+  overlay3
+);
+
+const popup4 = document.getElementById("benefits-4");
+const overlay4 = document.querySelector(".overlay");
+setupPopup(
+  document.getElementById("open-popup-4"),
+  document.getElementById("close-popup-4"),
+  popup4,
+  overlay4
+);
+
+// =====================
 //   PURCHASE SYSTEM
 // =====================
 
